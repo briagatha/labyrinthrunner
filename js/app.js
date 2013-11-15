51d38fb83ca941d19e9b759c4a0314e22c927a85
briagatha <breeyuka@gmail.com>
2013-11-14 21:36:18 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var load_page = function(pagename,data) {
+        $.get('../templates/'+pagename+'.html', function(templates) {
+            var template = $(templates).filter('#tpl-'+pagename).html();
+            $('#content').html(Mustache.render(template, data));
+        });
+    }
+    
+    load_page('welcome',{});
+    
+    $("#content").on('click','#start',function() {
+        load_page("level",{level: 1});
+    });
+
+    $("#content").on('click','#complete_labyrinth',function() {
+        var clevel = $(this).data('level');
+        $.get('../templates/next.html', function(templates) {
+            var template = $(templates).filter('#tpl-next').html();
+            $('#next_level_wrapper').html(Mustache.render(template, {level: clevel}));
+        });
+    });
+    
+    $("#content").on('click','#next_level',function() {
+        var nlevel = parseInt($(this).data('level')) + 1;
+        // TODO: Add logic for limiting the number of levels here
+        load_page("level",{level: nlevel}); 
+    });
+});
